2ab2f99871bf6ebe5b47440c0c98e0b32a9c3450
J Pratt <jp10010101010000@gmail.com>
2023-10-20 11:12:20 +1100

    Use layer names in layer switch code

--- keyboards/sofle/keymaps/cypher1/keymap.c
+++ keyboards/sofle/keymaps/cypher1/keymap.c
@@ -16,8 +16,8 @@
   */
 
 #include QMK_KEYBOARD_H
-#define L1_SPC LT(2, KC_SPC)
-#define L2_BSPC LT(3, KC_BSPC)
+#define LF_SPC LT(FUNCS, KC_SPC)
+#define LC_BSPC LT(CNTRL, KC_BSPC)
 #define DF_GAMES DF(GAMES)
 #define DF_QWERT DF(QWERT)
 #define MT_LSHN MT(MOD_LSFT, KC_ENT)
@@ -86,14 +86,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_TAB,     KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                         KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,   KC_EQL,
    KC_ESC,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                         KC_H,     KC_J,     KC_K,     KC_L,  KC_SCLN,  KC_QUOT,
   MT_LSHN,     KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,  KC_MUTE,  MS_BTN1,     KC_N,     KC_M,  KC_COMM,   KC_DOT,  KC_SLSH,  MT_RSHN,
-                      KC_BSLS,  KC_LALT,  KC_LGUI,  KC_LCTL,   L1_SPC,  L2_BSPC,   L1_SPC,  KC_PGUP,  KC_PGDN,  KC_BSLS
+                      KC_BSLS,  KC_LALT,  KC_LGUI,  KC_LCTL,   LF_SPC,  LC_BSPC,   LF_SPC,  KC_PGUP,  KC_PGDN,  KC_BSLS
 ),
 [GAMES] = LAYOUT(
    KC_GRV,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                         KC_6,     KC_7,     KC_8,     KC_9,     KC_0,  KC_MINS,
    KC_TAB,     KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                         KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,   KC_EQL,
    KC_ESC,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                         KC_H,     KC_J,     KC_K,     KC_L,  KC_SCLN,  KC_QUOT,
   KC_LSFT,     KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,  KC_MUTE,  MS_BTN1,     KC_N,     KC_M,  KC_COMM,   KC_DOT,  KC_SLSH,   KC_ENT,
-                      KC_BSLS,  KC_LALT,  KC_LGUI,  KC_LCTL,   KC_SPC,  KC_BSPC,   L1_SPC,  KC_PGUP,  KC_PGDN,  KC_BSLS
+                      KC_BSLS,  KC_LALT,  KC_LGUI,  KC_LCTL,   KC_SPC,  KC_BSPC,   LF_SPC,  KC_PGUP,  KC_PGDN,  KC_BSLS
 ),
 [FUNCS] = LAYOUT(
   _______,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,                        KC_F6,    KC_F7,    KC_F8,    KC_F9,   KC_F10,   KC_F11,
@@ -218,7 +218,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case KC_RCTL:
             update_pet_sneaking(record->event.pressed);
             break;
-        case L1_SPC:
+        case LF_SPC:
         case KC_SPC:
             if (record->event.pressed) {
               fn_held++;
