9a56f4dcfa54c64e69f84c31d44bd3f0f63c5d8b
Jay Pratt <jp10010101010000@gmail.com>
2024-09-02 13:48:22 +1000

    Add a delete key with FUNCS layer, make alt tab primary, re-add wasd arrows on CNTRL layer

--- keyboards/sofle/keymaps/cypher1/keymap.c
+++ keyboards/sofle/keymaps/cypher1/keymap.c
@@ -17,7 +17,7 @@
 
 #include QMK_KEYBOARD_H
 #define ALT_TAB_TERM 750
-#define LF_SPC LT(FUNCS, KC_SPC)
+#define LF_DEL LT(FUNCS, KC_DEL)
 #define LC_BSPC LT(CNTRL, KC_BSPC)
 #define MT_RSHN MT(MOD_RSFT, KC_ENT)
 #define MS_BTN1 KC_MS_BTN1
@@ -78,9 +78,9 @@ combo_t key_combos[] = {
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [QWERT] = { ENCODER_CCW_CW(KC_VOLU, KC_VOLD), ENCODER_CCW_CW(ALT_TAB, RALT_TAB) },
+    [QWERT] = { ENCODER_CCW_CW(KC_RGHT, KC_LEFT), ENCODER_CCW_CW(KC_DOWN, KC_UP) },
     [FUNCS] = { ENCODER_CCW_CW(MS_RGHT, MS_LEFT), ENCODER_CCW_CW(MS_DOWN, MS_UP) },
-    [CNTRL] = { ENCODER_CCW_CW(KC_RGHT, KC_LEFT), ENCODER_CCW_CW(KC_DOWN, KC_UP) },
+    [CNTRL] = { ENCODER_CCW_CW(KC_VOLU, KC_VOLD), ENCODER_CCW_CW(ALT_TAB, RALT_TAB) },
 };
 #endif
 
@@ -89,21 +89,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_GRV,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                         KC_6,     KC_7,     KC_8,     KC_9,     KC_0,  KC_MINS,
    KC_TAB,     KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                         KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,   KC_EQL,
    KC_ESC,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                         KC_H,     KC_J,     KC_K,     KC_L,  KC_SCLN,  KC_QUOT,
-  KC_LSFT,     KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,  KC_MUTE,  ALT_TAB,     KC_N,     KC_M,  KC_COMM,   KC_DOT,  KC_SLSH,  MT_RSHN,
-                      KC_BSLS,  KC_LALT,  KC_LGUI,  KC_LCTL,   KC_SPC,  LC_BSPC,   LF_SPC,  KC_PGUP,  KC_PGDN,  KC_BSLS
+  KC_LSFT,     KC_Z,     KC_X,     KC_C,     KC_V,     KC_B, RALT_TAB,  ALT_TAB,     KC_N,     KC_M,  KC_COMM,   KC_DOT,  KC_SLSH,  MT_RSHN,
+                      KC_BSLS,  KC_LALT,  KC_LGUI,  KC_LCTL,   KC_SPC,  LC_BSPC,   LF_DEL,  KC_PGUP,  KC_PGDN,  KC_BSLS
 ),
 [FUNCS] = LAYOUT(
   _______,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,                        KC_F6,    KC_F7,    KC_F8,    KC_F9,   KC_F10,   KC_F11,
   _______,  _______,  _______,  _______,  _______,  _______,                      MS_WLFT,   MS_WDN,   MS_WUP, MS_WRGHT,  _______,   KC_F12,
   _______,  _______,  _______,  _______,  KC_HOME,   KC_END,                      MS_LEFT,  MS_DOWN,    MS_UP,  MS_RGHT,  _______,  _______,
-  _______,  KC_MPRV,  KC_MPLY,  KC_MNXT,  _______,  _______, RALT_TAB,  ALT_TAB,  MS_BTN1,  MS_BTN3,  MS_BTN2,  _______,  _______,  _______,
-                      _______,  _______,  _______,  _______,  _______,   KC_DEL,  _______,  _______,  _______,  _______
+  _______,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  _______,  _______,  _______,  MS_BTN1,  MS_BTN3,  MS_BTN2,  _______,  _______,  _______,
+                      _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
 ),
 [CNTRL] = LAYOUT(
   _______,  _______,  _______,  _______,  _______,  _______,                      _______,  _______,  _______,  KC_LBRC,  KC_RBRC,  _______,
-  _______,  _______,  _______,  _______,  _______,  _______,                      _______,  _______,  _______,  _______,  _______,  _______,
-  _______,  _______,  _______,  _______,  _______,  _______,                      KC_LEFT,  KC_DOWN,    KC_UP,  KC_RGHT,  _______,  _______,
-  _______,  KC_MPRV,  KC_MPLY,  KC_MNXT,  _______,  CM_TOGG, RALT_TAB,  ALT_TAB,  _______,  _______,  _______,  _______,  _______,  _______,
+  _______,  _______,    KC_UP,  _______,  _______,  _______,                      _______,  _______,  _______,  _______,  _______,  _______,
+  _______,  KC_LEFT,  KC_DOWN,  KC_RGHT,  _______,  _______,                      KC_LEFT,  KC_DOWN,    KC_UP,  KC_RGHT,  _______,  _______,
+  _______,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
                       _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
 )
 };
@@ -247,7 +247,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case KC_RCTL:
             update_pet_sneaking(pressed);
             break;
-        case LF_SPC:
         case KC_SPC:
             if (pressed) {
               fn_held++;
